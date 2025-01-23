ec00cd2bbfd68de0292074cdcc36bc1cd9789d87
MinaNagy99 <minanagykhalefa@gmail.com>
2025-01-23 12:10:44 +0200

    add more images projects in slider ( Home Page )

--- src/components/HomePage/signatureProject/slider/Slider.jsx
+++ src/components/HomePage/signatureProject/slider/Slider.jsx
@@ -41,26 +41,6 @@ function Slider() {
       page: "/arup",
     },
     {
-      src: "/signatureProjects/arup-datta/p12.webp",
-      sectionName: "walden",
-      page: "/arup",
-    },
-    {
-      src: "/signatureProjects/arup-datta/p32.webp",
-      sectionName: "Callaghan",
-      page: "/arup",
-    },
-    {
-      src: "/signatureProjects/arup-datta/p42.webp",
-      sectionName: "Lexus",
-      page: "/arup",
-    },
-    {
-      src: "/signatureProjects/arup-datta/p43.webp",
-      sectionName: "Lexus",
-      page: "/arup",
-    },
-    {
       src: "/signatureProjects/wolsey/p11.webp",
       sectionName: "Aqsarniit",
       page: "/wolsey",
@@ -92,14 +72,54 @@ function Slider() {
       sectionName: "CKE",
       page: "/wolsey",
     },
-    {
-      src: "/signatureProjects/wolsey/p11.webp",
-      sectionName: "CKE",
-      page: "/wolsey",
-    },
 
     {
       src: "/signatureProjects/wolsey/p41.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/1.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/2.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/3.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/4.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/5.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/6.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/7.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/8.webp",
+      sectionName: "CKE",
+      page: "/wolsey",
+    },
+    {
+      src: "/signatureProjects/9.webp",
       sectionName: "CKE",
       page: "/wolsey",
     },
@@ -113,7 +133,7 @@ function Slider() {
         centeredSlides={true}
         preventClicks={true}
         autoplay={{
-          delay: 2500, // Set autoplay delay in milliseconds (3 seconds)
+          delay: 3200, // Set autoplay delay in milliseconds (3 seconds)
           disableOnInteraction: false, // Continue autoplay even after user interaction
         }}
         coverflowEffect={{
@@ -145,16 +165,11 @@ function Slider() {
         {images.map((img, index) => (
           <SwiperSlide key={index} style={{ padding: "10px" }}>
             {/* Padding to make space for the thick border */}
-            <div
-              // onClick={() => {
-              //   navigateWithScroll(img.page, img.sectionName);
-              // }}
-              className="relative border-x-[8px] border-white rounded-lg overflow-hidden"
-            >
+            <div className="relative border-x-[8px] border-white rounded-lg overflow-hidden">
               {/* Thicker white border */}
               <div className="overlay absolute inset-0  bg-black opacity-50 transition-opacity"></div>
               <img
-                className="w-full scallUpOnHover object-cover "
+                className="w-full scallUpOnHover  h-96 object-cover "
                 src={img.src}
                 alt={`Slide ${index + 1}`}
               />
